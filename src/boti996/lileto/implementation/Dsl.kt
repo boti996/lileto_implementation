802f8fc52cd3bb62f91f9c17da6e8a22c19789db
boti996 <dobai.botond@gmail.com>
2019-12-17 14:30:45 +0100

    dsl: improved

--- src/boti996/lileto/implementation/Dsl.kt
+++ src/boti996/lileto/implementation/Dsl.kt
@@ -2,10 +2,69 @@ package boti996.lileto.implementation
 
 import boti996.lileto.implementation.models.Bracket
 import boti996.lileto.implementation.models.LiletoContext
+import boti996.lileto.implementation.models.TextContext
+import boti996.lileto.implementation.parser.SpecialCharacter
 
 @DslMarker
 annotation class LiletoDsl
 
+/**
+ * Special character bracket
+ * @constructor initialize with one or multiple special characters
+ */
+@LiletoDsl
+data class SpecialCharBuilder(val chars: List<SpecialCharacter>) {
+    /** @param char special character */
+    constructor(char: SpecialCharacter): this(listOf(char))
+
+    fun build() = TextContext(chars.joinToString("") {
+            specialCharacter -> specialCharacter.character()
+    })
+}
+
+/**
+ * Text bracket
+ * */
+@LiletoDsl
+data class TextBuilder(val text: String = "") {
+    private val texts = mutableListOf<TextContext>()
+
+    operator fun String.unaryPlus() {
+        texts += TextContext(this)
+    }
+
+    fun spec(chars: List<SpecialCharacter>, setup: SpecialCharBuilder.() -> Unit) {
+        val specialCharBuilder = SpecialCharBuilder(chars)
+        specialCharBuilder.setup()
+        texts += specialCharBuilder.build()
+    }
+
+    fun spec(char: SpecialCharacter, setup: SpecialCharBuilder.() -> Unit) {
+        val specialCharBuilder = SpecialCharBuilder(char)
+        specialCharBuilder.setup()
+        texts += specialCharBuilder.build()
+    }
+
+    fun build() = TextContext("$text${texts.joinToString("") { it.evaluate() }}")
+}
+
+
+@LiletoDsl
+class command {
+    // TODO
+}
+
+
+@LiletoDsl
+class container {
+    // TODO
+}
+
+
+@LiletoDsl
+class template {
+    // TODO
+}
 
 /**
  * It contains all the other brackets
@@ -17,6 +76,8 @@ class LiletoContextBuilder {
     operator fun Bracket.unaryPlus() {
         children += this
     }
+
+    //TODO
 
     fun build() = LiletoContext.evaluate(children)
 
